1d1242726d398106291195054e93bfc8f83babc7
ahmed-fenni <ahmedfenny8@gmail.com>
2020-03-28 09:55:09 +0100

    solved

--- warmUp14.js
+++ warmUp14.js
@@ -7,3 +7,17 @@
 // array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 // array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 // array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+function isSubset(arr,sup){
+	var result =[]
+	for(i=0;i<arr.length;i++){
+		for(j=0;j<sup.length;j++){
+			if(arr[i]===sup[j]){
+				result.push(sup[j]) 
+			}
+		}    
+	}
+   if(result.length===sup.length){
+   	return true
+   }
+    return false
+}
